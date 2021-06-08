ede7c8fb502d1c7e4cc2be77bcc931b9afb95992
gmatei <mateicosmingoanta@gmail.com>
2021-06-08 18:55:57 +0300

    Started Battle implementation

--- js/battle.js
+++ js/battle.js
@@ -1,11 +1,74 @@
-var sec = 0;
-    function pad ( val ) { return val > 9 ? val : "0" + val; }
-    setInterval( function(){
-        document.getElementById("seconds").innerHTML=pad(++sec%60);
-        document.getElementById("minutes").innerHTML=pad(parseInt(sec/60,10));
-    }, 1000);
+let hero = null;
+let SetOfQuestions = null;
+let username = getCookie("USERNAME");
+let token = getCookie("TOKEN");
 
-function on() {
-    document.getElementById("overlay").style.display = "block";
+const heroName = new URLSearchParams(window.location.search).get('hero'); // get the hero's name (provided in the url)
+
+function init() {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', '../api/hero/getByName.php?hero=' + heroName, false);
+    xhr.setRequestHeader("X-Auth-Username", username);
+    xhr.setRequestHeader("X-Auth-Token", token);
+    xhr.onload = function() {
+        console.log(this);
+        if (this.status == 200) {
+            result = JSON.parse(this.responseText)['result'];
+            if (result === "invalid-hero-name") {
+                document.querySelector('html').innerHTML = "Invalid hero name.";    
+            } else {
+                hero = JSON.parse(this.responseText)['responseBody'];
+                setBackground();
+                setItem1();
+                setItem4();
+                startGame();
+            }
+        }
+    }
+    xhr.send();
 }
 
+function setBackground() {
+
+    document.querySelector('html').style.backgroundImage = "url(../images/" + hero.background_url + ")";
+}
+
+function setItem1() {
+
+    var text = [];
+
+    text.push(
+    `
+    <h2> ${hero.hero_name} </h2>
+    <p> <b>${hero.domain}: </b> ${hero.ability_name} </p>   
+    `
+    )
+
+    document.querySelector('div.item1').innerHTML = text.join("");
+}
+
+function setItem4() {
+
+    var text = [];
+
+    text.push(
+    `
+    <img src="../images/${hero.photo_url}" alt="${hero.hero_name}" id="superhero"></img> 
+    `
+    )
+
+    document.querySelector('div.item4').innerHTML = text.join("");
+}
+
+
+function startGame(){
+
+
+    
+
+
+}
+
+
+
+init();
