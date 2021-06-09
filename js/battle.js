9dcc50fd6c7d041bc411e421ae782d38dfe0378f
gmatei <mateicosmingoanta@gmail.com>
2021-06-09 10:25:38 +0300

    Finalized battle mode (pending minor bug fixes)

--- js/battle.js
+++ js/battle.js
@@ -1,6 +1,24 @@
 let hero = null;
 let username = getCookie("USERNAME");
 let token = getCookie("TOKEN");
+let questionCount = 0;
+let easySet = null;
+let mediumSet = null;
+let hardSet = null;
+let expertSet = null;
+let currentSet = null;
+let buttonListener = true;
+let lifeCount = 5;
+let score = 0;
+let time = 0;
+let sec = 0;
+let gameOver = false;
+let physicsFlag = false;
+let historyFlag = false;
+let mathFlag = false;
+let chemistryFlag = false;
+let compSciFlag = false;
+let artFlag = false;
 
 const heroName = new URLSearchParams(window.location.search).get('hero'); // get the hero's name (provided in the url)
 
@@ -18,9 +36,10 @@ function init() {
             } else {
                 hero = JSON.parse(this.responseText)['responseBody'];
                 setBackground();
-                setItem1();
-                setItem4();
-                startGame();
+                setItem1(); //hero name and ability
+                setItem4(); //hero picture
+                displayScore(); //item 6
+                displayLife(); //item 8
             }
         }
     }
@@ -62,26 +81,278 @@ function setItem4() {
 
 function startGame()
 {
+    returnQuestionsOfDifficulty('1');
+    returnQuestionsOfDifficulty('2');
+    returnQuestionsOfDifficulty('3');
+    returnQuestionsOfDifficulty('4');
 
-    let easySet = returnQuestionsOfDifficulty('1');
-    let mediumSet = returnQuestionsOfDifficulty('2');
-    let hardSet = returnQuestionsOfDifficulty('3');
-    let expertSet = returnQuestionsOfDifficulty('4');
+    document.querySelector(".button").addEventListener("click", activateAbility);
 
-    while(!gameOver())
-    {
-        
+    setInterval( function(){
+        if(!gameOver && !compSciFlag)
+        {document.getElementById("seconds").innerHTML=pad(++sec%60);
+        document.getElementById("minutes").innerHTML=pad(parseInt(sec/60,10));
+        time++;}
+    }, 1000);   //set time
 
-
-    }
-
+    initQuestion()
 
 }
+
+function activateAbility() 
+{
+    if(currentSet[questionCount%10].domain == hero.domain)
+        {   
+            useAbility();
+            window.alert("Ability has been used!");
+        }
+}
+
+function useAbility()
+{
+    if(hero.domain == 'Math') 
+    {
+        mathFlag = true;
+    }
+    if(hero.domain == 'Geography') 
+    {
+        let correctAnswer = currentSet[questionCount%10].correct_answer;
+
+        if(correctAnswer == 'a') {document.getElementById('b').innerHTML = ` `; document.getElementById('d').innerHTML = ` `;}
+        if(correctAnswer == 'b') {document.getElementById('a').innerHTML = ` `; document.getElementById('c').innerHTML = ` `;}
+        if(correctAnswer == 'c') {document.getElementById('b').innerHTML = ` `; document.getElementById('d').innerHTML = ` `;}
+        if(correctAnswer == 'd') {document.getElementById('a').innerHTML = ` `; document.getElementById('c').innerHTML = ` `;}
+        
+    }
+    if(hero.domain == 'History') 
+    {
+        historyFlag = true;
+    }
+    if(hero.domain == 'Biology') 
+    {
+        time += 20;
+        sec += 20;
+        initQuestion();
+    }
+    if(hero.domain == 'Chemistry') 
+    {
+        chemistryFlag = true;
+    }
+    if(hero.domain == 'Physics') 
+    {
+        physicsFlag = true;
+    }
+    if(hero.domain == 'Computer Science') 
+    {
+        compSciFlag = true;
+    }
+    if(hero.domain == 'Sports') 
+    {
+        score += 800;
+        gameOverLogic();
+    }
+    if(hero.domain == 'Arts') 
+    {
+        artFlag = true;
+    }
+    if(hero.domain == 'Literature') 
+    {
+        let nmb = Math.floor(Math.random() * 10);
+
+        if(nmb == 0) {mathFlag = true;}
+        if(nmb == 1) {let correctAnswer = currentSet[questionCount%10].correct_answer;
+                    if(correctAnswer == 'a') {document.getElementById('b').innerHTML = ` `; document.getElementById('d').innerHTML = ` `;}
+                    if(correctAnswer == 'b') {document.getElementById('a').innerHTML = ` `; document.getElementById('c').innerHTML = ` `;}
+                    if(correctAnswer == 'c') {document.getElementById('b').innerHTML = ` `; document.getElementById('d').innerHTML = ` `;}
+                    if(correctAnswer == 'd') {document.getElementById('a').innerHTML = ` `; document.getElementById('c').innerHTML = ` `;}}
+        if(nmb == 2) {historyFlag = true;}
+        if(nmb == 3) {time += 20;
+                    sec += 20;
+                    initQuestion();}
+        if(nmb == 4) {chemistryFlag = true;}
+        if(nmb == 5) {physicsFlag = true;}
+        if(nmb == 6) {compSciFlag = true;}
+        if(nmb == 7) {score += 800;
+                     gameOverLogic();}
+        if(nmb == 8) {artFlag = true;}
+        if(nmb == 9) {window.alert("Call the all-knowing Peterca Adrian : 0751640092");}
+    }
+
+}
+
+
+function displayScore()
+{
+    document.querySelector('div.item6').innerHTML = `<h1> Score: </h1> <p> ${score} </p>`;  
+}
+
+function displayLife()
+{
+    if(lifeCount == 5)
+        {document.getElementById("heart1").style.opacity = 1; document.getElementById("heart2").style.opacity = 1; document.getElementById("heart3").style.opacity = 1; document.getElementById("heart4").style.opacity = 1; document.getElementById("heart5").style.opacity = 1;}
+    if(lifeCount == 4)
+        {document.getElementById("heart1").style.opacity = 1; document.getElementById("heart2").style.opacity = 1; document.getElementById("heart3").style.opacity = 1; document.getElementById("heart4").style.opacity = 1; document.getElementById("heart5").style.opacity = 0.2;}
+    if(lifeCount == 3)
+        {document.getElementById("heart1").style.opacity = 1; document.getElementById("heart2").style.opacity = 1; document.getElementById("heart3").style.opacity = 1; document.getElementById("heart4").style.opacity = 0.2; document.getElementById("heart5").style.opacity = 0.2;}
+    if(lifeCount == 2)
+        {document.getElementById("heart1").style.opacity = 1; document.getElementById("heart2").style.opacity = 1; document.getElementById("heart3").style.opacity = 0.2; document.getElementById("heart4").style.opacity = 0.2; document.getElementById("heart5").style.opacity = 0.2;}
+    if(lifeCount == 1)
+        {document.getElementById("heart1").style.opacity = 1; document.getElementById("heart2").style.opacity = 0.2; document.getElementById("heart3").style.opacity = 0.2; document.getElementById("heart4").style.opacity = 0.2; document.getElementById("heart5").style.opacity = 0.2;} 
+    if(lifeCount == 0)
+        {document.getElementById("heart1").style.opacity = 0.2; document.getElementById("heart2").style.opacity = 0.2; document.getElementById("heart3").style.opacity = 0.2; document.getElementById("heart4").style.opacity = 0.2; document.getElementById("heart5").style.opacity = 0.2;}    
+}
+
+function waitForClientToAnswer()
+{
+    document.querySelectorAll('.answer').forEach(function(e) {
+        e.addEventListener('click', function() {
+
+            if(buttonListener)
+                {
+                let correctAnswer = currentSet[questionCount%10].correct_answer;    
+                
+                if (this.id == correctAnswer)    
+                    {
+                        this.style.backgroundColor = "#2f6631";
+                        
+                        if(currentSet[questionCount%10].difficulty == '1')  {score += 100; if(physicsFlag) {score += 100; physicsFlag = false;}}
+                        if(currentSet[questionCount%10].difficulty == '2')  {score += 250; if(physicsFlag) {score += 250; physicsFlag = false;}}
+                        if(currentSet[questionCount%10].difficulty == '3')  {score += 400; if(physicsFlag) {score += 400; physicsFlag = false;}}
+                        if(currentSet[questionCount%10].difficulty == '4')  {score += 600; if(physicsFlag) {score += 600; physicsFlag = false;}}
+                    
+                        if(historyFlag)
+                        {
+                            lifeCount++;
+                            historyFlag = false;
+                        }
+
+                        if(mathFlag)
+                        {
+                            lifeCount+=2;
+                            mathFlag = false;
+                        }
+
+                        if(chemistryFlag)
+                        {
+                            time-=10;
+                            sec-=10;
+                            chemistryFlag = false;
+                        }
+
+                        if(artFlag)
+                        {
+                            score+=600;
+                            artFlag = false;
+                        }
+                    }
+                    else
+                    {
+                        this.style.backgroundColor = "red";
+                        colorCorrectAnswer(correctAnswer);
+                        lifeCount--;
+
+                        if(mathFlag)
+                        {
+                            lifeCount--;
+                            mathFlag = false;
+                        }   
+
+                        if(artFlag)
+                        {
+                            score-=600;
+                            artFlag = false;
+                        }
+                    }
+                
+                
+                displayScore();
+                displayLife();
+                buttonListener = false;
+
+                document.getElementById("arrow").addEventListener("click", nextQuestion);
+                compSciFlag = false;
+                }        
+                
+      });
+    });  
+}
+
+function nextQuestion() 
+{
+    if(!buttonListener)
+        initQuestion();
+}
+
+function initQuestion()
+{
+    if(questionCount == 40 || lifeCount == 0)
+        gameOverLogic();
+
+
+    resetAnswerColor();
+    buttonListener = true;
+    questionCount++;
+    displayQuestionNumber();
+
+    if(questionCount <= 10) currentSet = easySet;
+    else if(questionCount <= 20)    currentSet = mediumSet;
+    else if(questionCount <= 30)    currentSet = hardSet;
+    else if(questionCount <= 40)    currentSet = expertSet;
+
+    displayQuestionText(currentSet);
+    displayQuestionAnswers(currentSet);
+
+    waitForClientToAnswer();
+}
+
+function resetAnswerColor()
+{
+    document.getElementById("a").style.backgroundColor = "#d6d6d67a";
+    document.getElementById("b").style.backgroundColor = "#d6d6d67a";
+    document.getElementById("c").style.backgroundColor = "#d6d6d67a";
+    document.getElementById("d").style.backgroundColor = "#d6d6d67a";
+
+}
+
+function colorCorrectAnswer(correctAnswer)
+{
+    if(correctAnswer == 'a') document.getElementById("a").style.backgroundColor = "#2f6631";
+    else if(correctAnswer == 'b') document.getElementById("b").style.backgroundColor = "#2f6631";
+    else if(correctAnswer == 'c') document.getElementById("c").style.backgroundColor = "#2f6631";
+    else if(correctAnswer == 'd') document.getElementById("d").style.backgroundColor = "#2f6631";
+
+}
+
+function displayQuestionNumber()
+{
+    document.querySelector('div.item3').innerHTML = `<h1> ${questionCount}/40 </h1>`;
+}
+
+function displayQuestionText(currentSet)
+{
+    document.querySelector('div.item2').innerHTML = `<p> <b>${currentSet[questionCount%10].domain}:</b> ${currentSet[questionCount%10].content} </p>`;
+}
+
+function displayQuestionAnswers(currentSet)
+{
+    document.getElementById('a').innerHTML = `${currentSet[questionCount%10].a}`;
+    document.getElementById('b').innerHTML = `${currentSet[questionCount%10].b}`;
+    document.getElementById('c').innerHTML = `${currentSet[questionCount%10].c}`;
+    document.getElementById('d').innerHTML = `${currentSet[questionCount%10].d}`;
+}
+
+
 
 function returnQuestionsOfDifficulty(difficulty)
 {
     let xhr = new XMLHttpRequest();
-    xhr.open('GET', '../api/question/getWithDifficulty.php?difficulty=' + difficulty, true);
+    
+    if(difficulty === '1')
+        xhr.open('GET', '../api/question/getWithDifficulty.php?difficulty=' + difficulty, false);
+        else
+        xhr.open('GET', '../api/question/getWithDifficulty.php?difficulty=' + difficulty, true);
+    
+    
     xhr.setRequestHeader("X-Auth-Username", username);
     xhr.setRequestHeader("X-Auth-Token", token);
     xhr.onload = function() {
@@ -96,13 +367,86 @@ function returnQuestionsOfDifficulty(difficulty)
                 }
             else
             {
-                return JSON.parse(this.responseText)['set'];
+                if(difficulty === '1')
+                    easySet = JSON.parse(this.responseText)['questionSet'];
+                if(difficulty === '2')
+                    mediumSet = JSON.parse(this.responseText)['questionSet'];
+                if(difficulty === '3')
+                    hardSet = JSON.parse(this.responseText)['questionSet'];
+                if(difficulty === '4')
+                    expertSet = JSON.parse(this.responseText)['questionSet'];
+                    
             }
         }
     }
     xhr.send();
 }
 
+function pad ( val ) { return val > 9 ? val : "0" + val; }
 
+
+function gameOverLogic()
+{
+    gameOver = true;
+
+    let scoreMultiplier = 2.0;
+    if(time < 1000) scoreMultiplier = 1.0;
+    if(time < 900) scoreMultiplier = 1.1;
+    if(time < 800) scoreMultiplier = 1.2;
+    if(time < 700) scoreMultiplier = 1.3;
+    if(time < 600) scoreMultiplier = 1.4;
+    if(time < 500) scoreMultiplier = 1.5;
+    if(score < 1000) scoreMultiplier = 1.0;
+
+    let lifeMultiplier = 1.0;
+    if(lifeCount == 5) lifeMultiplier = 1.5;
+    if(lifeCount == 4) lifeMultiplier = 1.4;
+    if(lifeCount == 3) lifeMultiplier = 1.3;
+    if(lifeCount == 2) lifeMultiplier = 1.2;
+    if(lifeCount == 1) lifeMultiplier = 1.1;
+
+    let bonus = 0;
+    if(questionCount >= 10) bonus += 500;
+    if(questionCount >= 20) bonus += 1000;
+    if(questionCount >= 30) bonus += 1500;
+    if(questionCount >= 40) bonus += 2000;
+
+    score = parseInt(score * scoreMultiplier * lifeMultiplier) + bonus;
+
+    //addEntryIntoLeaderboard();
+
+    let minutes = parseInt(time / 60);
+    let seconds = time % 60;
+
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("ov-text-1").innerHTML = `Game Over!`;
+
+    if(seconds < 10)
+        document.getElementById("ov-text-2").innerHTML = `Final Score: ${score} <br> Time: ${minutes}:0${seconds}`;
+        else
+        document.getElementById("ov-text-2").innerHTML = `Final Score: ${score} <br> Time: ${minutes}:${seconds}`;
+
+    document.getElementById("try-again-button").innerHTML = `<button class="ov-retry-button" type="button" onclick="document.location.href='../html/battle.html?hero=${heroName}'">Try Again</button>`;
+
+}
+
+function addEntryIntoLeaderboard()
+{
+    let xhr = new XMLHttpRequest();
+
+    let url = '../api/leaderboard/add.php?username=' + username + '&score=' + score + '&hero=' + heroName; 
+
+    xhr.open('POST', url, true);
+    xhr.setRequestHeader("X-Auth-Username", username);
+    xhr.setRequestHeader("X-Auth-Token", token);
+    xhr.onload = function() {
+        console.log(this);
+        if (this.status != 200)
+            window.alert("An error occured while trying to add your score to the database! Please screenshot your result and contact an admin!");
+    }
+
+    xhr.send();
+}
 
 init();
+startGame();
